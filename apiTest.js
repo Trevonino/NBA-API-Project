d2aefa307fdb8d276002418c69db9d642dc69b69
Trevonino <sneedtrev@gmail.com>
2023-03-31 20:43:49 -0500

    Updated API with new info endpoint Added info search functionality to page

--- apiTest.js
+++ apiTest.js
@@ -1,12 +1,6 @@
 let baseUrl = "https://trevonino.pythonanywhere.com/api/"
 testData = [];
-
-async function getAllPlayers() {
-    newURL = baseUrl + "players"
-    const response = await fetch(newURL);
-    var data = await response.json();
-    console.log(data);
-}
+infoData = [];
 
 async function searchPlayerName() {
     newURL = baseUrl + 'player?'
@@ -31,10 +25,21 @@ async function searchPlayerName() {
         for (i = 0; i < data.length; i++) {
             textID = "result" + i;
             document.getElementById("searchResults").innerHTML +=
-                "<p id='" + textID + "'>" + data[i].full_name + "</p>";
+                "<p id='" + textID + "' onclick='getStats(" + data[i].id + ");'>" + data[i].full_name + "</p>";
         }
     }
     testData = data;
     console.log(testData);
     document.getElementById("loader").style.display = "none";
+}
+
+async function getStats(playerID){
+    document.getElementById("loader").style.display = "block";
+    newURL = baseUrl + 'info?id=' + playerID;
+    const response = await fetch(newURL);
+    var data = await response.json();
+    infoData = data;
+    alertMessage = "Full Name: " + infoData[0] + "\nBirthday: " + infoData[1].slice(0,10) + "\nTeam: " + infoData[2] + ", " + infoData[3] + "\nHeight: " + infoData[4] + "\nWeight: " + infoData[5] + "\nPosition: " + infoData[6] + "\nPlayed From: " + infoData[7] + " - " + infoData[8] + "\nDrafted Round: " + infoData[9] + "\nDrafted Number: " + infoData[10];
+    document.getElementById("loader").style.display = "none";
+    window.alert(alertMessage);
 }
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import sqlite3
 
 # SELECT team_name, position FROM common_player_info WHERE person_id = (SELECT id FROM player WHERE full_name = 'Bill Laimbeer');
 
+
 def connect_to_db():
     conn = sqlite3.connect('basketball.sqlite')
     return conn
@@ -42,7 +43,7 @@ def get_player(fname, lname):
         cur = conn.cursor()
         print(fname)
         print(lname)
-        cur.execute("SELECT * FROM player WHERE first_name LIKE ? AND last_name LIKE ? ORDER BY id;",
+        cur.execute("SELECT * FROM player WHERE LOWER(first_name) LIKE LOWER(?) AND LOWER(last_name) LIKE LOWER(?) ORDER BY id",
                     (fname, lname))
         rows = cur.fetchall()
 
@@ -62,6 +63,39 @@ def get_player(fname, lname):
     return players
 
 
+def get_stats(player_id):
+    info = []
+    try:
+        conn = connect_to_db()
+        print('we connect')
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        print('we try to run command')
+        cur.execute("SELECT * FROM common_player_info WHERE person_id = ?;",
+                    (player_id,))
+        print('we get a row')
+        row = cur.fetchone()
+        print('we get a row')
+        info.append(row["display_first_last"])
+        info.append(row["birthdate"])
+        info.append(row["team_name"])
+        info.append(row["team_city"])
+        info.append(row["height"])
+        info.append(row["weight"])
+        info.append(row["position"])
+        info.append(row["from_year"])
+        info.append(row["to_year"])
+        info.append(row["draft_round"])
+        info.append(row["draft_number"])
+        print(row["display_first_last"])
+
+    except:
+        info = []
+        print(player_id)
+
+    return info
+
+
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
@@ -73,9 +107,15 @@ def api_get_players():
 
 @app.route('/api/player', methods=['GET'])
 def player():
-    fname  = request.args.get('fname', '%')
-    lname  = request.args.get('lname', '%')
+    fname = request.args.get('fname', '%')
+    lname = request.args.get('lname', '%')
     return jsonify(get_player(fname, lname))
+
+
+@app.route('/api/info', methods=['GET'])
+def info():
+    id = request.args.get('id', '%')
+    return jsonify(get_stats(id))
 
 
 if __name__ == '__main__':
